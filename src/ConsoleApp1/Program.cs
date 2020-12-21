09888a5c26b2ae8664e97aac4a4a9ed74d01271e
Ryan McElroy <rmcelroy@hylaine.com>
2020-12-21 17:09:55 -0500

    Initial commit and encryptions

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -5,12 +5,6 @@ using System.Text;
 
 public class Kata
 {
-    private static int _number;
-    private static double _productSum = 0;
-    private static LinkedList<int> _productList = new LinkedList<int>();
-    private static LinkedListNode<int> _median;
-    private static List<int> _productTracker = new List<int>();
-
     public static void Main()
     {
         var input = "";
@@ -20,7 +14,70 @@ public class Kata
             input = Console.ReadLine();
             if (input == "e")
                 return;
+            var encrypted = Encrypt("WEAREDISCOVEREDFLEEATONCE", 3);
+            Console.WriteLine($"{encrypted}");
             Console.ReadLine();
         }
+    }
+
+    /*
+     Do-while loop for string
+    Use modulus for number of rails to determine increment or decrement for SB index
+    if (0 or mod0) 
+        toggle = !toggle
+    Inc or Dec
+     */
+    public static string Encrypt(string str, int numberOfRails)
+    {
+        var rails = new Dictionary<int, StringBuilder>();
+        for (int i = 0; i < numberOfRails; i++) 
+        {
+            rails.Add(i, new StringBuilder());
+        }
+        var ascend = false;
+        var railIndex = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (railIndex == 0 || railIndex % numberOfRails == (numberOfRails - 1))
+                ascend = !ascend;
+            rails[railIndex].Append(str[i]);
+            railIndex = (ascend == true) ?
+                railIndex + 1 : railIndex -1;
+        }
+        for (int i = 1; i < numberOfRails; i++)
+        {
+            rails[0].Append(rails[i]);
+        }
+        return rails[0].ToString();
+    }
+
+    //Dis gon be harder
+    /*
+     * To build Rail 0: str[0] + str[ (numberOfRails -2) + 1 + (numberOfRails -2) ]
+     * To build Rail [numRails]: str[numRails-1] + str[(numRails -2) + 1 + (numRails -2)]
+     * To build Rail 1: str[1] + str[ ]
+     */
+    public static string Decrypt(string str, int numRails)
+    {
+        var rails = new Dictionary<int, StringBuilder>();
+        for (int i = 0; i < numRails; i++)
+        {
+            rails.Add(i, new StringBuilder());
+        }
+        var ascend = false;
+        var railIndex = 0;
+        for (int i = str.Length; i < str.Length; i++)
+        {
+            if (railIndex == 0 || railIndex % numRails == (numRails - 1))
+                ascend = !ascend;
+            rails[railIndex].Append(str[i]);
+            railIndex = (ascend == true) ?
+                railIndex + 1 : railIndex - 1;
+        }
+        for (int i = 1; i < numRails; i++)
+        {
+            rails[0].Append(rails[i]);
+        }
+        return rails[0].ToString();
     }
 }
