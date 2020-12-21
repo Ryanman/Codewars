6bfffe44119fc63ea86d1485c680fa34099a5fbe
Ryan McElroy <rmcelroy@hylaine.com>
2020-12-21 11:31:08 -0500

    Correct

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -5,11 +5,18 @@ using System.Text;
 
 public class Kata
 {
-    private static int _number;
-    private static double _productSum = 0;
-    private static LinkedList<int> _productList = new LinkedList<int>();
-    private static LinkedListNode<int> _median;
-    private static List<int> _productTracker = new List<int>();
+    private static List<string> _codes = new List<string>
+    {
+        "F3RF5LF7",
+        "FFFR345F2LL"
+    };
+
+    private static Dictionary<char, string> _colors = new Dictionary<char, string>
+    {
+        { 'F',"pink" },
+        { 'L',"red" },
+        { 'R',"green" }
+    };
 
     public static void Main()
     {
@@ -20,7 +27,66 @@ public class Kata
             input = Console.ReadLine();
             if (input == "e")
                 return;
+
+            foreach (var code in _codes)
+            {
+                Console.WriteLine($"Code: {code}\r\n\t{Highlight(code)}");
+            }
             Console.ReadLine();
+
         }
+    }
+
+    public static string Highlight(string code)
+    {
+        var sb = new StringBuilder();
+        var colors = new Dictionary<char, string>
+        {
+            { 'F',"pink" },
+            { 'L',"red" },
+            { 'R',"green" },
+            { '(',"" },
+            { ')',"" }
+        };
+        for (int i = 0; i < 10; i++)
+        {
+            colors.Add(i.ToString()[0], "orange");
+        }
+        var previousColor = "start";
+        code = "(" + code;
+        for (int i = 1; i < code.Length; i++)
+        {
+            var color = colors[code[i]];
+            if (color != previousColor && color != "")
+            {
+                sb.Append($"</span><span style=\"color: {color}\">");
+            }
+            if (color == ""
+                && previousColor != "")
+            {
+                sb.Append("</span>");
+            }
+            sb.Append(code[i]);
+            if (i == code.Length - 1 && previousColor != "")
+            {
+                sb.Append("</span>");
+            }
+            previousColor = color;
+        }
+        //foreach (var ch in code)
+        //{
+        //    var color = colors[ch];
+        //    if (color == "" && previousColor != "")
+        //    {
+        //        sb.Append("</span>");
+        //    }
+        //    if (color != previousColor && color != "")
+        //    {
+        //        sb.Append($"</span><span style=\"color: {color}\">");
+        //    }
+        //    sb.Append(ch);
+        //    previousColor = color;
+        //}
+        return sb.ToString(7,sb.Length-7);
     }
 }
