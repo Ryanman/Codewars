3adce4cac3bf35eaa6fb3f9f26521e18b1011113
Ryan McElroy <rmcelroy@hylaine.com>
2021-08-11 09:55:42 -0400

    Basic test success

--- Program.cs
+++ Program.cs
@@ -5,22 +5,85 @@ using System.Text;
 
 public class Kata
 {
-    private static int _number;
-    private static double _productSum = 0;
-    private static LinkedList<int> _productList = new LinkedList<int>();
-    private static LinkedListNode<int> _median;
-    private static List<int> _productTracker = new List<int>();
+    private static int _dim;
+    private static bool[,]_map;
+    private static string _maze;
+
 
     public static void Main()
     {
-        var input = "";
-        while (true)
+        string a = ".W.\n" +
+                   ".W.\n" +
+                   "...",
+
+               b = ".W.\n" +
+                   ".W.\n" +
+                   "W..",
+
+               c = "......\n" +
+                   "......\n" +
+                   "......\n" +
+                   "......\n" +
+                   "......\n" +
+                   "......",
+
+               d = "......\n" +
+                   "......\n" +
+                   "......\n" +
+                   "......\n" +
+                   ".....W\n" +
+                   "....W.";
+
+        Console.WriteLine($"Path for a possible? {PathFinder(a)}");
+        Console.WriteLine($"Path for a possible? {PathFinder(b)}");
+        Console.WriteLine($"Path for a possible? {PathFinder(c)}");
+        Console.WriteLine($"Path for a possible? {PathFinder(d)}");        
+        Console.ReadLine();
+        //Assert.AreEqual(true, Finder.PathFinder(a));
+        //Assert.AreEqual(false, Finder.PathFinder(b));
+        //Assert.AreEqual(true, Finder.PathFinder(c));
+        //Assert.AreEqual(false, Finder.PathFinder(d));
+    }
+
+
+    public static bool PathFinder(string maze)
+    {
+        _dim = maze.IndexOf('\n');
+        _map = new bool[_dim,_dim];
+        _maze = maze.Replace("\n","");
+        var good = Search(0, 0);
+        
+        return good;
+    }
+
+    private static bool Search(int i, int j)
+    {
+        if (i >= _dim || j >= _dim || i < 0 || j < 0)
+            return false;
+        if (i == _dim - 1 && j == _dim - 1)
+            return true;        
+        var mazeDef = _maze[(i * _dim) + j];
+        if (_map[i,j] //Already visited
+            || mazeDef == 'W')
+            return false;
+        _map[i, j] = true; //Mark as visited
+        //Right, Down, Left, Up
+        if (Search(i , j + 1) == true)
         {
-            Console.WriteLine($"Enter Number, or E for Exit");
-            input = Console.ReadLine();
-            if (input == "e")
-                return;
-            Console.ReadLine();
+            return true;
         }
+        if (Search(i + 1, j) == true)
+        {
+            return true;
+        }
+        if (Search(i, j -1) == true)
+        {
+            return true;
+        }
+        if (Search(i -1, j) == true)
+        {
+            return true;
+        }
+        return false;
     }
 }
