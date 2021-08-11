79b7f4fe069ff3856a39f4dd48b7439dfc125a9d
Ryan McElroy <rmcelroy@hylaine.com>
2021-08-11 13:35:09 -0400

    Cycle Code

--- Program.cs
+++ Program.cs
@@ -6,36 +6,38 @@ using System.Text;
 public class Kata
 {
     private static int _dim;
-    private static Dictionary<string,bool> _map;
+    private static Dictionary<string,bool[]> _map; //E,S,W,N
     private static string _maze;
 
     public static bool PathFinder(string maze)
     {
         _dim = maze.IndexOf('\n');
-        _map = new Dictionary<string, bool>();
+        _map = new Dictionary<string, bool[]>();
         _maze = maze.Replace("\n","");
-        return Search(0, 0);
+        return Search(0, 0, 4);
     }
 
-    private static bool Search(int i, int j)
+    private static bool Search(int i, int j, int direction)
     {
         if (i >= _dim || j >= _dim || i < 0 || j < 0)
             return false;
         if (i == _dim - 1 && j == _dim - 1)
             return true;
         var key = $"{i},{j}";
-        if (_map.ContainsKey(key) //Already visited
+        if (!_map.ContainsKey(key))        
+            _map.Add(key, new bool[5]);        
+        if (_map[key][direction] //Already visited
             || _maze[(i * _dim) + j] == 'W')
-            return false;
-        _map.Add(key, true);//Mark as visited
-        //Right, Down, Left, Up
-        if (Search(i , j + 1) == true)
+            return false;        
+        _map[key][direction] = true;//Mark as visited
+        //E,S,W,N
+        if (Search(i , j + 1, 0) == true)
             return true;        
-        if (Search(i + 1, j) == true)        
+        if (Search(i + 1, j, 1) == true)        
             return true;        
-        if (Search(i, j -1) == true)        
+        if (Search(i, j -1, 2) == true)        
             return true;        
-        if (Search(i -1, j) == true)        
+        if (Search(i -1, j, 3) == true)        
             return true;        
         return false;
     }
@@ -74,6 +76,13 @@ public class Kata
                 "W....W\n" +
                 "W.WWWW\n" +
                 "W.....\n" +
+                "...W..",
+
+            g = "......\n" +
+                ".W.W.W\n" +
+                ".W...W\n" +
+                ".W.WWW\n" +
+                ".W....\n" +
                 "...W..";
 
         Console.WriteLine($"Path for a possible? {PathFinder(a)}");
@@ -82,6 +91,7 @@ public class Kata
         Console.WriteLine($"Path for d possible? {PathFinder(d)}");
         Console.WriteLine($"Path for e possible? {PathFinder(e)}");
         Console.WriteLine($"Path for f possible? {PathFinder(f)}");
+        Console.WriteLine($"Path for g possible? {PathFinder(g)}");
         Console.ReadLine();
     }
 }
