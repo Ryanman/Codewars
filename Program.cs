f6fcd987aebdc9cb80d01ff6d6f4bfe07c170c6c
Ryan McElroy <rmcelroy@hylaine.com>
2021-08-12 16:56:18 -0400

    Math fixes

--- Program.cs
+++ Program.cs
@@ -5,9 +5,13 @@ using System.Text;
 
 public class Kata
 {
-    private static int _number;
-    private static double _productSum = 0;
-    private static LinkedList<int> _productList = new LinkedList<int>();
+    private static long _number;
+    private static int _numUniqueChars = 0;
+    private static Dictionary<char, int> _characters = new Dictionary<char, int>()
+    {
+        {'A',0},
+        {'B',0}
+    };
     private static LinkedListNode<int> _median;
     private static List<int> _productTracker = new List<int>();
 
@@ -27,15 +31,62 @@ public class Kata
 
     public static long ListPosition(string value)
     {
-        return 1;
+        
+        foreach (var c in value)
+        {
+            if (_characters[c] == 0)
+                _numUniqueChars++;
+            _characters[c]++;
+        }
+        var maxNumChars = _characters.Values.Max(); //Do the thing to simplify factorials here if there are errors https://www.chilimath.com/lessons/intermediate-algebra/dividing-factorials/
+        
+        return AddPositions(value);
+    }
+
+    private static long AddPositions(string value)
+    {
+        if (value.Length == 1)
+            return 1;
+        var lchar = value[0];
+        long denom = 1;
+        foreach (var c in _characters)
+        {
+            denom *= Factorial(c.Value == 0 ? 1 : c.Value);
+        }
+        var numWords = Factorial(value.Length)
+            / denom;
+
+        var partitionIndex = 0;
+        foreach (var c in _characters)
+        {
+            if (c.Key == lchar)
+                break;
+            if (c.Value > 0)
+                partitionIndex++;
+        }
+
+        var partitionValue = (partitionIndex / _numUniqueChars) * numWords;
+        _number += partitionValue;
+        _characters[lchar]--;
+        if (_characters[lchar] == 0)
+            _numUniqueChars--;
+        return _number + AddPositions(value.Substring(1));
+    }
+
+    private static long Factorial(long n)
+    {
+        if (n == 0)
+            return 1;
+        return n * Factorial(n - 1);
     }
 
     public static void Main()
     {
         Tuple<string, int>[] testCases =
         {
-            new Tuple<string,int>("A",1),
+            
             new Tuple<string,int>("ABAB",2),
+            new Tuple<string,int>("A",1),
             new Tuple<string,int>("AAAB",1),
             new Tuple<string,int>("BAAA",4),
             new Tuple<string,int>("QUESTION",24527),
