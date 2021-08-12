da55474d56ea161f14027fd0d26fa25bd5cd7a15
Ryan McElroy <rmcelroy@hylaine.com>
2021-08-12 15:56:09 -0400

    Algo writing

--- Program.cs
+++ Program.cs
@@ -11,16 +11,42 @@ public class Kata
     private static LinkedListNode<int> _median;
     private static List<int> _productTracker = new List<int>();
 
+    /*
+    Combinations = word.len! / ((#LetterX!)(#LetterY!).....)
+    */
+
+    /*
+     * Assuming 300 words
+     * Index algo =
+     * foreach letter
+     *  # of partitions (ABC = 3 partitions)
+     *  Get to 0-based index for that partition (B = 100, A = 0, C = 200)
+     *  Decrement number of letters available in ordered dic
+     *  Recurse?
+     */
+
+    public static long ListPosition(string value)
+    {
+        return 1;
+    }
+
     public static void Main()
     {
-        var input = "";
-        while (true)
+        Tuple<string, int>[] testCases =
         {
-            Console.WriteLine($"Enter Number, or E for Exit");
-            input = Console.ReadLine();
-            if (input == "e")
-                return;
-            Console.ReadLine();
+            new Tuple<string,int>("A",1),
+            new Tuple<string,int>("ABAB",2),
+            new Tuple<string,int>("AAAB",1),
+            new Tuple<string,int>("BAAA",4),
+            new Tuple<string,int>("QUESTION",24527),
+            new Tuple<string,int>("BOOKKEEPER",10743),
+        };
+
+        foreach (var tc in testCases)
+        {
+            var position = ListPosition(tc.Item1);
+            Console.WriteLine($"Word: {tc.Item1}\r\n\tExpected: {tc.Item2}\r\n\tActual: {position}");
         }
+        
     }
 }
