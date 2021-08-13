cb28fdf7b4173e1ceedd1efe02d5192b2b3de630
Ryan McElroy <rmcelroy@hylaine.com>
2021-08-13 10:48:20 -0400

    Debug

--- Program.cs
+++ Program.cs
@@ -11,16 +11,42 @@ public class Kata
     private static LinkedListNode<int> _median;
     private static List<int> _productTracker = new List<int>();
 
+    public static bool ValidateBattlefield(int[,] field)
+    {
+        // Write your magic here
+        return true;
+    }
+
     public static void Main()
     {
-        var input = "";
-        while (true)
+        List<int[,]> fields = new List<int[,]>() { 
+            new int[10, 10]
+                     {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+                      {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+                      {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                      {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                      {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                      {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}} 
+        };
+        foreach (var field in fields)
         {
-            Console.WriteLine($"Enter Number, or E for Exit");
-            input = Console.ReadLine();
-            if (input == "e")
-                return;
-            Console.ReadLine();
+            Console.WriteLine("Field:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("\t");
+                for(int j = 0; j < 10; j++)
+                {
+                    Console.Write($"field[i, j] ");
+                }
+                Console.WriteLine();
+            }
+            var result = ValidateBattlefield(field);
+            Console.WriteLine($"Result: {result}");
         }
+        Console.ReadLine();
     }
 }
