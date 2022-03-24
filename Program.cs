627d1195c5f70e0513f5f6fcaac97c6521576f8a
Ryan McElroy <rmcelroy@hylaine.com>
2022-03-23 22:30:47 -0400

    Additional Test case added

--- Program.cs
+++ Program.cs
@@ -24,6 +24,18 @@ public class Kata
             });
             _genericInput.Add(new int[,] //1
             {
+                { 1,0,0},
+                { 1,2,0},
+                { 1,2,0}
+            });
+            _genericInput.Add(new int[,] //1
+            {
+                { 1,1,1},
+                { 0,2,2},
+                { 0,0,0}
+            });
+            _genericInput.Add(new int[,] //1
+            {
                 { 1,0,1},
                 { 0,1,2},
                 { 2,2,1}
@@ -56,6 +68,9 @@ public class Kata
     public static int IsSolved(int[,] board)
     {
         var completed = true;
+        Console.WriteLine($"{board[0, 0]}-{board[0, 1]}-{board[0, 2]}\r\n" +
+            $"{board[1, 0]}-{board[1, 1]}-{board[1, 2]}\r\n" +
+            $"{board[2, 0]}-{board[2, 1]}-{board[2, 2]}");
         for (int i = 0; i < 3; i++)
         {
             var corner = board[i, i];
