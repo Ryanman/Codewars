edebfe313a3e1492aa658812567322950d51beb3
Ryan McElroy <Rmcelroy90@gmail.com>
2022-10-21 15:48:04 -0400

    Minor success

--- Program.cs
+++ Program.cs
@@ -41,7 +41,21 @@ public class Kata
             //  {2, 8, 7, 4, 1, 9, 6, 3, 5},
             //  {3, 4, 5, 2, 8, 6, 1, 7, 9}
             //};
+
             var res = ValidateSolution(goodBoard);
+
+            Console.WriteLine($"{res}");
+            var badBoard = new int[9][];
+            badBoard[0] = new int[] { 5, 3, 4, 6, 7, 8, 9, 1, 2 };
+            badBoard[0] = new int[] { 6, 7, 2, 1, 9, 0, 3, 4, 8 };
+            badBoard[0] = new int[] { 1, 0, 0, 3, 4, 2, 5, 6, 0 };
+            badBoard[0] = new int[] { 8, 5, 9, 7, 6, 1, 0, 2, 0 };
+            badBoard[0] = new int[] { 4, 2, 6, 8, 5, 3, 7, 9, 1 };
+            badBoard[0] = new int[] { 7, 1, 3, 9, 2, 4, 8, 5, 6 };
+            badBoard[0] = new int[] { 9, 0, 1, 5, 3, 7, 2, 1, 4 };
+            badBoard[0] = new int[] { 2, 8, 7, 4, 1, 9, 6, 3, 5 };
+            badBoard[0] = new int[] { 3, 0, 0, 4, 8, 1, 1, 7, 9 };
+            res = ValidateSolution(badBoard);
             Console.WriteLine($"{res}");
             input = Console.ReadLine();
             if (input == "e")
@@ -56,17 +70,16 @@ public class Kata
         var map = new Dictionary<Tuple<string, int>, BitArray>();
         var mapTypes = new string[] { "row","col","square" };
         for (int i = 0; i < 9; i++)
-        {
-            foreach (var mapType in mapTypes)
-            {
-                map[new Tuple<string, int>(mapType, i)] = new BitArray(9, false);
-            }
+        {            
             for(int j = 0; j < 9; j++)
             {
                 var value = board[i][j];
-                foreach (var mapType in mapTypes)
+                if (i == 0)
                 {
-                    var key = new Tuple<string, int>(mapType, i);
+                    foreach (var mapType in mapTypes)
+                    {
+                        map[new Tuple<string, int>(mapType, j)] = new BitArray(10, false);
+                    }
                 }
                 var rowKey = new Tuple<string, int>("row", i);
                 var colKey = new Tuple<string, int>("col", j);
