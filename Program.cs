72a4018f094efea7ebd5f09c4c813ad6b264d73c
Ryan McElroy <rmcelroy@hylaine.com>
2023-10-16 11:42:40 -0400

    Minor commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ public class Kata
     private static LinkedList<int> _productList = new LinkedList<int>();
     private static LinkedListNode<int> _median;
     private static List<int[,]> _genericInput = new List<int[,]>();
+    private static StringBuilder sb;
 
     public static void Main()
     {
@@ -20,7 +21,30 @@ public class Kata
             input = Console.ReadLine();
             if (input == "e")
                 return;
+            Decompose(Convert.ToInt32(input));
             Console.ReadLine();
         }
+    }
+
+    public static string Decompose(long n)
+    {
+        sb = new StringBuilder();
+        DecomposeHelper(n - 1, n * n);
+        return "Nothing";
+    }
+
+    public static string DecomposeHelper (long n, long remainder)
+    {
+        if (n == 0) return "1";
+        if (n < 1) return "Nothing";
+        var square = n * n;
+        string result;
+        if (remainder - square > 1)
+            result = DecomposeHelper(n, remainder - square);
+        else
+            result = DecomposeHelper(n - 1, remainder);
+        if (result == "Nothing") return result;
+        return result + " ";
+
     }
 }
